e399a65a2282dc7c5884a7266df3194a201cf5b1
root <root@ip-172-31-44-131.ap-south-1.compute.internal>
2023-05-18 16:49:08 +0000

    commit fot icici project

---
+++ src/main/java/com/ICICIBANK/www/App.java
@@ -0,0 +1,13 @@
+package com.ICICIBANK.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello this is vishwa icici bank project  World!" );
+    }
+}
